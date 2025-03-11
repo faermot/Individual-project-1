0877e46f9087af08e293353ccb9733e046a6b4d2
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2025-03-11 20:08:29 +0300

    Добавьте файлы проекта.

---
+++ WpfApp25/Core/ConnectionCore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp25.Core
+{
+    public class ConnectionCore
+    {
+        public static Frame? CoreFrame { get; set; }
+    }
+}
---
+++ WpfApp25/View/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp25.Core;
+using WpfApp25.View.Pages;
+
+namespace WpfApp25
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectionCore.CoreFrame = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
